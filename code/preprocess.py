d93535ecf14f9cf0d7d80d1a5ffc4871b00c83e3
Swapnil Singhavi <cd_svsanya@yahoo.co.in>
2018-10-30 20:50:33 -0400

    update

--- code/preprocess.py
+++ code/preprocess.py
@@ -7,6 +7,9 @@ from openslide.deepzoom import DeepZoomGenerator
 import math
 import os
 import numpy as np
+from skimage.color import rgb2gray
+from skimage.feature import canny
+from skimage.morphology import binary_closing, binary_dilation, disk
 
 
 def open_slide(slide_name, folder):
@@ -151,3 +154,67 @@ def generate_slide_indices(slide_name, folder, tile_size=256, overlap=1):
     return tile_indices
 
 
+def keep_tile(tile_tuple, tile_size, tissue_threshold):
+    """
+    Determine if a tile should be kept.
+    This filters out tiles based on size and a tissue percentage
+    threshold, using a custom algorithm. If a tile has height &
+    width equal to (tile_size, tile_size), and contains greater
+    than or equal to the given percentage, then it will be kept;
+    otherwise it will be filtered out.
+    Args:
+    tile_tuple: A (slide_num, tile) tuple, where slide_num is an
+      integer, and tile is a 3D NumPy array of shape
+      (tile_size, tile_size, channels).
+    tile_size: The width and height of a square tile to be generated.
+    tissue_threshold: Tissue percentage threshold.
+    Returns:
+    A Boolean indicating whether or not a tile should be kept for
+    future usage.
+    """
+    slide_num, tile = tile_tuple
+    if tile.shape[0:2] == (tile_size, tile_size):
+        tile_orig = tile
+
+    # Check 1
+    # Convert 3D RGB image to 2D grayscale image, from
+    # 0 (dense tissue) to 1 (plain background).
+        tile = rgb2gray(tile)
+    # 8-bit depth complement, from 1 (dense tissue)
+    # to 0 (plain background).
+        tile = 1 - tile
+    # Canny edge detection with hysteresis thresholding.
+    # This returns a binary map of edges, with 1 equal to
+    # an edge. The idea is that tissue would be full of
+    # edges, while background would not.
+        tile = canny(tile)
+    # Binary closing, which is a dilation followed by
+    # an erosion. This removes small dark spots, which
+    # helps remove noise in the background.
+        tile = binary_closing(tile, disk(10))
+    # Binary dilation, which enlarges bright areas,
+    # and shrinks dark areas. This helps fill in holes
+    # within regions of tissue.
+        tile = binary_dilation(tile, disk(10))
+    # Fill remaining holes within regions of tissue.
+        tile = binary_fill_holes(tile)
+    # Calculate percentage of tissue coverage.
+        percentage = tile.mean()
+        check1 = percentage >= tissue_threshold
+
+    # Check 2
+    # Convert to optical density values
+        tile = optical_density(tile_orig)
+    # Threshold at beta
+        beta = 0.15
+        tile = np.min(tile, axis=2) >= beta
+    # Apply morphology for same reasons as above.
+        tile = binary_closing(tile, disk(2))
+        tile = binary_dilation(tile, disk(2))
+        tile = binary_fill_holes(tile)
+        percentage = tile.mean()
+        check2 = percentage >= tissue_threshold
+
+        return check1 and check2
+    else:
+        return False
